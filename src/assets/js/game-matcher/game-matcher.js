a9dc98f89fa83dd92302ef2d9872129271e0b565
Shafi Goldwasser;1958- <jarne.losschaert@student.howest.be>
2022-03-30 17:17:53 +0200

    create and join game

--- src/assets/js/game-matcher/game-matcher.js
+++ src/assets/js/game-matcher/game-matcher.js
@@ -2,14 +2,36 @@
 
 function processConnectionForm(e) {
     e.preventDefault();
-    const name = document.querySelector('#name').value;
-    const amount = document.querySelector('#amount').value;
-    console.log(name, amount);
-    gameExistChecker(amount);
+    const playerName = document.querySelector('#name').value;
+    const amount = parseInt(document.querySelector('#amount').value);
+    console.log(playerName, amount);
+    
+    gameExistChecker(amount, playerName);
 }
 
-function gameExistChecker(amount) {
+function gameExistChecker(amount, playerName) {
     fetchFromServer(`/games?prefix=${_config.gamePrefix}&numberOfPlayers=${amount}&started=false`, 'GET')
-        .then(res => console.log(res))
-        .catch(errorHandler);
+        .then(games => {
+            if (games.length === 0) {
+                console.log('new game');
+                const bodyParams = {
+                    'prefix': _config.gamePrefix,
+                    'numberOfplayers': amount
+                };
+                fetchFromServer('/games', 'POST', bodyParams).then(game => {
+                    joinGame(game.id, playerName)
+                });
+        } else {
+            console.log('connect')
+
+            const firstGame = games[0];
+            joinGame(firstGame.id, playerName)
+        }});
+} 
+
+function joinGame(gameId, playerName){
+    const requestBody = {
+        'playerName' : playerName
+    }
+    fetchFromServer(`/games/${gameId}/players`, 'POST', requestBody).then(res => console.log(res));
 }
