dbacc382af1ae6de2626d703fddacaf4eaa952ce
jean-baptiste.van.parys <jeanBaptiste.van.parys@student.howest.be>
2022-03-31 14:57:58 +0200

    made the default popup function

--- src/assets/js/popup.js
+++ src/assets/js/popup.js
@@ -1,5 +1,17 @@
 "use strict";
 
-function defaultPopup() {
-    
+function defaultPopup(headertitle, title, message, button, buttons=[]) {
+    const $template = document.querySelector("#default").content.firstElementChild.cloneNode(true);
+    $template.querySelector("header h2").innerText = headertitle;
+    $template.querySelector(".popup-content h2").innerText = title;
+    $template.querySelector(".popup-content form p").innerText = message;
+    $template.querySelector(".submit-btns input").setAttribute("value", button);
+    console.log(buttons)
+    buttons.forEach(btn => {
+        const $btn = $template.querySelector(".submit-btns input").cloneNode();
+        console.log(btn.text);
+        $btn.setAttribute("value", btn.text);
+        $template.querySelector(".submit-btns").insertAdjacentElement("beforeend", $btn);
+    });
+    document.querySelector("body").insertAdjacentElement("beforeend", $template);
 }
