9ebc0cf4722b89557ab8275418a2b7bb52c3df0e
jean-baptiste.van.parys <jeanBaptiste.van.parys@student.howest.be>
2022-03-31 16:58:16 +0200

    made the showDicePopup, showRolledDicePopup

--- src/assets/js/initialiser.js
+++ src/assets/js/initialiser.js
@@ -1,17 +1,14 @@
 "use strict";
 let _token = null;
+let _body;
 
 document.addEventListener('DOMContentLoaded',init);
 
 function init(){
+    _body = document.querySelector("body");
     testConnection();
-    defaultPopup("header title ","leuke titel ","jaaa man");
 }
 
-function testf(e){
-    e.preventDefault()
-    console.log("test");
-}
 function testConnection(){
     fetchFromServer('/tiles','GET').then(tiles => console.log(tiles)).catch(errorHandler);
 }
--- src/assets/js/popup.js
+++ src/assets/js/popup.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function defaultPopup(headertitle, title, message, buttons=[{text:"continue",function: hidePopup }]) {
+function showDefaultPopup(headertitle, title, message, buttons = [{ text: "continue", function: closePopup }]) {
     const $template = document.querySelector("#default").content.firstElementChild.cloneNode(true);
     $template.querySelector("header h2").innerText = headertitle;
     $template.querySelector(".popup-content h2").innerText = title;
@@ -12,10 +12,24 @@ function defaultPopup(headertitle, title, message, buttons=[{text:"continue",fun
         $btn.addEventListener("click", btn.function);
         $template.querySelector(".submit-btns").insertAdjacentElement("beforeend", $btn);
     });
-    document.querySelector("body").insertAdjacentElement("beforeend", $template);
+    _body.insertAdjacentElement("beforeend", $template);
 }
 
-function hidePopup(e){
+function showDicePopup(funct) {
+    const $template = document.querySelector("#dice").content.firstElementChild.cloneNode(true);
+    $template.querySelector("input[type='submit']").addEventListener("click", funct);
+    _body.insertAdjacentElement("beforeend", $template);
+}
+
+function showRolledDicePopup(number, fucnt) {
+    const $template = document.querySelector("#roll-dice").content.firstElementChild.cloneNode(true);
+    $template.querySelector(".roll-dice form p").innerText = number;
+    console.log($template.querySelector(".roll-dice form input"));
+    $template.querySelector(".roll-dice form input").addEventListener("click", closePopup);
+    _body.insertAdjacentElement("beforeend", $template);
+}
+
+function closePopup(e) {
     e.preventDefault();
-    e.target.closest("article").remove()
+    e.target.closest("article").remove();
 }
