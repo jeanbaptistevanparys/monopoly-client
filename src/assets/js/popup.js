d58b74869aabb9f61c8b4eef42318284968e3242
jean-baptiste.van.parys <jeanBaptiste.van.parys@student.howest.be>
2022-04-01 12:17:54 +0200

    made the showtitledeedpopup

--- src/assets/js/popup.js
+++ src/assets/js/popup.js
@@ -30,7 +30,7 @@ function showRolledDicePopup(number, funct) {
     _body.insertAdjacentElement("beforeend", $template);
 }
 
-function showPlayerInfoPopup(playername,properties=[{name: 'Mediterranean', position: 1, cost: 60, mortgage: 30, rent: 2,color:"PURPLE"}]) {
+function showPlayerInfoPopup(playername, properties) {
     const $template = document.querySelector("#playerinfo").content.firstElementChild.cloneNode(true);
     $template.querySelector(".playerpopup header h2").innerText = playername;
     $template.querySelector(".playerpopup-content h2").innerText = playername;
@@ -39,11 +39,49 @@ function showPlayerInfoPopup(playername,properties=[{name: 'Mediterranean', posi
     $template.querySelector(".playerpopup .properties section").remove()
     properties.forEach(propertie => {
         let $propcopy = $prop.cloneNode(true);
-        console.log($propcopy);
         $propcopy.querySelector("h3").innerText = propertie.name;
-        $propcopy.querySelector("h3").style.backgroundColor = propertie.color; 
+        $propcopy.querySelector("h3").style.backgroundColor = propertie.color;
         $propcopy.querySelector("p").innerHTML = `<span class="striketrough">M</span> ${propertie.cost}`;
         $template.querySelector(".playerpopup .properties").insertAdjacentElement("beforeend", $propcopy);
+    });
+    _body.insertAdjacentElement("beforeend", $template);
+}
+
+function showTitledeedPopup(streetname, prop = {
+    "name": "Mediterranean",
+    "position": 1,
+    "cost": 60,
+    "mortgage": 30,
+    "rent": 2,
+    "rentWithOneHouse": 10,
+    "rentWithTwoHouses": 30,
+    "rentWithThreeHouses": 90,
+    "rentWithFourHouses": 160,
+    "rentWithHotel": 250,
+    "housePrice": 50,
+    "streetColor": "PURPLE",
+    "groupSize": 2,
+    "color": "PURPLE",
+    "type": "street",
+    "nameAsPathParameter": "Mediterranean"
+}) {
+    const $template = document.querySelector("#titledeed").content.firstElementChild.cloneNode(true);
+    $template.querySelector(".titledeed header h2").innerText = "Titledeed";
+    $template.querySelector(".titledeed-content h2").innerText = "STREETNAME";
+    $template.querySelector(".titledeed .icons div").addEventListener("click", closePopup);
+    const $prop = $template.querySelectorAll(".titledeed-content .values p");
+    $template.querySelectorAll(".titledeed-content .values p").forEach(e => e.remove());
+    let keylist = ["rentWithOneHouse","rentWithTwoHouses","rentWithThreeHouses","rentWithFourHouses","rentWithHotel","housePrice"]
+    Object.keys(prop).forEach(key => {
+        if (keylist.includes(key)) {
+            let p1 = $prop[0].cloneNode(true);
+            p1.innerText = `-${key}`
+            let p2 = $prop[1].cloneNode(true);
+            p2.innerHTML = `<span class="striketrough">M</span> ${prop[key]}`
+            console.log(key, prop[key])
+            $template.querySelector(".titledeed-content .values").insertAdjacentElement("beforeend", p1);
+            $template.querySelector(".titledeed-content .values").insertAdjacentElement("beforeend", p2);
+        }
     });
     _body.insertAdjacentElement("beforeend", $template);
 }
