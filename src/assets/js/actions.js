086dbbef8b7acc89e354d5b58d995c4972adb2f1
guust.taillieu <guust.taillieu@student.howest.be>
2022-04-16 18:36:42 +0200

    Import current tile + little css changes.

--- src/assets/js/actions.js
+++ src/assets/js/actions.js
@@ -7,38 +7,39 @@ function testConnection() {
 
 function defaultActions(gameState) {
 	let playerInfo = gameState.players.filter(player => player.name == playerName)[0];
-	let playerCurrentCardIndex = getIndexOfCardByName(playerInfo.currentTile);
+	let playerCurrentTileIndex = getIndexOfTileByName(playerInfo.currentTile);
 
-	importNextTwelve(playerCurrentCardIndex);
+	importCurrentTile(playerCurrentTileIndex);
+	importNextTwelveTiles(playerCurrentTileIndex);
 }
 
-function importNextTwelve(currentTileIndex) {
+function importCurrentTile(currentTileIndex) {
+	let propertyCard = makePropertyCard(currentTileIndex);
+	document.querySelector('.property-card').innerHTML = '';
+	document.querySelector('.property-card').insertAdjacentElement('beforeend', propertyCard);
+}
+
+function importNextTwelveTiles(currentTileIndex) {
 	for (let i = currentTileIndex + 1; i < currentTileIndex + 13; i++) {
-		const $template = document.querySelector('#property-template').content.firstElementChild.cloneNode(true);
-		if (!allTiles[i].color || allTiles[i].color == 'WHITE' || allTiles[i].type == 'railroad') {
-			$template.querySelector('h3').style.color = 'BLACK';
-		}
-		if (allTiles[i].housePrice) {
-			$template.querySelector('h3').innerText = allTiles[i].name;
-			$template.querySelector('h3').style.backgroundColor = allTiles[i].color;
-			$template.querySelector('p').innerHTML = `<span class="striketrough">M</span> ${allTiles[i].cost}`;
-		} else {
-			$template.querySelector('.player').classList.add(trimSpaces(allTiles[i].type).toLowerCase());
-			$template.classList.add('special');
-			$template.querySelector('h3').innerText = allTiles[i].type;
-		}
-		document.querySelector('.nextTwelve').insertAdjacentElement('beforeend', $template);
+		let propertyCard = makePropertyCard(i);
+		document.querySelector('.nextTwelve').insertAdjacentElement('beforeend', propertyCard);
 	}
 }
 
-function trimSpaces(string) {
-	return string.split(' ').join('');
-}
-
-function getIndexOfCardByName(cardName) {
-	for (let i = 0; i < allTiles.length; i++) {
-		if (allTiles[i].name == cardName) {
-			return i;
-		}
+function makePropertyCard(tileIndex) {
+	let tile = allTiles[tileIndex];
+	const $template = document.querySelector('#property-template').content.firstElementChild.cloneNode(true);
+	if (!tile.color || tile.color == 'WHITE' || tile.type == 'railroad') {
+		$template.querySelector('h3').style.color = 'BLACK';
 	}
+	if (tile.housePrice) {
+		$template.querySelector('h3').innerText = tile.name;
+		$template.querySelector('h3').style.backgroundColor = tile.color;
+		$template.querySelector('p').innerHTML = `<span class="striketrough">M</span> ${tile.cost}`;
+	} else {
+		$template.querySelector('.player').classList.add(trimSpaces(tile.type).toLowerCase());
+		$template.classList.add('special');
+		$template.querySelector('h3').innerText = tile.type;
+	}
+	return $template;
 }
--- src/assets/js/initialiser.js
+++ src/assets/js/initialiser.js
@@ -11,15 +11,19 @@ let _body;
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
+	testConnection();
+
 	_token = loadFromStorage(_config.localStorageToken);
 	gameId = loadFromStorage(_config.localStorageGameId);
 	playerName = loadFromStorage(_config.localStoragePlayer);
 
-	getTiles().then(tiles => (allTiles = tiles)).catch(errorHandler);
-
-	testConnection();
-
 	gameStartedChecker = setInterval(getCurrentGameState, _config.delay);
+	getTiles()
+		.then(tiles => {
+			allTiles = tiles;
+			getCurrentGameState();
+		})
+		.catch(errorHandler);
 }
 
 function getCurrentGameState() {
