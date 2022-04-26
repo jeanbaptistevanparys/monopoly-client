f90fcc32fce15d19a37c9d00b5f7797ce059037c
Shafi Goldwasser;1958- <jarne.losschaert@student.howest.be>
2022-04-26 16:25:40 +0200

    validate html player-info

---
+++ src/assets/js/player-info.js
@@ -0,0 +1,66 @@
+'use strict';
+
+function importPLayerInfo() {
+    const thisPlayer = getThisPlayer();
+    const properties = thisPlayer.properties;
+    displayName();
+    displayerNumberOfHousesAndHotels(properties);
+    displayNumberOfProperties(properties);
+    displayProperties(properties);
+}
+
+function getThisPlayer() {
+    let thisPlayer;
+    for (const playerIndex in currentGameState.players) {
+        if (currentGameState.players[playerIndex].name == playerName) {
+            thisPlayer = currentGameState.players[playerIndex];
+        }
+    }
+    return thisPlayer;
+}
+
+function displayName() {
+    qs('#player-info h2').innerText = playerName;
+}
+
+function displayerNumberOfHousesAndHotels(properties) {
+    let numberOfHouses = 0;
+    let numberOfHotels = 0;
+    for (const propertyIndex in properties) {
+        if (properties[propertyIndex].houseCount) {
+            numberOfHouses += properties[propertyIndex].houseCount;
+        }
+        if (properties[propertyIndex].hotelCount) {
+            numberOfHotels += properties[propertyIndex].hotelCount;
+        }
+    }
+    qs('#player-info .info p:nth-of-type(1) em').innerText = numberOfHouses;
+    qs('#player-info .info p:nth-of-type(2) em').innerText = numberOfHotels;
+}
+
+function displayNumberOfProperties(properties) {
+    qs('#player-info .info p:nth-of-type(3) em').innerText = properties.length;
+}
+
+function displayMoney(thisPlayer) {
+    qs('#player-info .info .money').innerText = '';
+    qs('#player-info .info .money').insertAdjacentHTML('beforeend', `<span class="striketrough">M</span>${thisPlayer.money}`);
+}
+
+
+function displayProperties(properties) {
+    qs('#player-info .properties').innerHTML = '';
+    console.log(properties);
+    for (const propertyIndex in properties) {
+        const $template = qs('#player-property').content.firstElementChild.cloneNode(true);
+        getTile(properties[propertyIndex].property).then(res => {
+            const tile = res;
+            console.log(tile);
+            $template.querySelector('h3').innerText = tile.name;
+            $template.querySelector('p').innerHTML = `<span class="striketrough">M</span> ${tile.cost}`;
+            $template.style.backgroundColor = 'WHITE';
+            $template.querySelector('h3').style.backgroundColor = tile.color;
+        })
+        qs('#player-info .properties').insertAdjacentElement('beforeend', $template);
+    }
+}
